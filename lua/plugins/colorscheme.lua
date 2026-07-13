c8400adb4de4736d07280d9e365473ec0bf7430e
4bderrahmane <me@4bderrahmane.com>
2026-07-13 01:13:15 +0100

    an update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "EdenEast/nightfox.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "sainnhe/everforest" },
+  { "sainnhe/gruvbox-material" },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "carbonfox",
     },
   },
 }
